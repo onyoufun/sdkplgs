c19aeac3f5d62ce64fb14878c6d0c5c995a9bb05
youney <tomjingjing@163.com>
2023-03-10 19:53:53 +0800

    Revert "fyrestar登出接口"
    This reverts commit d2fa36a550678d4759cf3f9b62e99f837c9fcddb.

--- fyrestar/src/main/java/com/linxcool/sdkface/fyrestar/FyrestarInterface.java
+++ fyrestar/src/main/java/com/linxcool/sdkface/fyrestar/FyrestarInterface.java
@@ -118,11 +118,6 @@ public class FyrestarInterface extends YmnPluginWrapper {
         });
     }
 
-    @YFunction(name = "fyrestar_logout")
-    public void onLogout() {
-        FYSDK.getInstance().logout();
-    }
-
     @YFunction(name = "fyrestar_pay")
     public void onPay(String productId, String productName, String orderId, String roleId, String roleName, String payType, String ext) {
         FoyoOrderParam op = new FoyoOrderParam();
